c32c3f1740b8402fdfd72c9ca0115a3c963b761e
SexyBug <2921978909@qq.com>
2019-06-14 15:05:06 +0800

    修复cost

--- src/main/java/sexygroup/spring/pojo/Cost.java
+++ src/main/java/sexygroup/spring/pojo/Cost.java
@@ -2,11 +2,10 @@ package sexygroup.spring.pojo;
 
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Data
